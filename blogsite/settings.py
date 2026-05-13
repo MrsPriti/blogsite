0bbfa3bd8a48f87681b1b81ccc91c60d2c8c312e
Sourav Pradhan <sourava2345@gmail.com>
2026-05-13 22:03:32 +0530

    Switch to Supabase PostgreSQL database

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "blogsite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "MyBlog@123%",
+        "HOST": "db.dzoamkdtjpeqsehtsdvm.supabase.co",
+        "PORT": "5432",
     }
 }
 
