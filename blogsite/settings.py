8ae7ddd1d62918f79c98c7a0b5144584a7987ea6
Sourav Pradhan <sourava2345@gmail.com>
2026-05-13 22:30:24 +0530

    Use Supabase connection pooler to fix Vercel connectivity

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -85,10 +85,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
-        "USER": "postgres",
+        "USER": "postgres.dzoamkdtjpeqsehtsdvm",
         "PASSWORD": "MyBlog@123%",
-        "HOST": "db.dzoamkdtjpeqsehtsdvm.supabase.co",
-        "PORT": "5432",
+        "HOST": "aws-0-ap-south-1.pooler.supabase.com",
+        "PORT": "6543",
     }
 }
 
