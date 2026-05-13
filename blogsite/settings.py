5d099e8e8b11e8e089b43426be9f777b58af1c46
Sourav Pradhan <sourava2345@gmail.com>
2026-05-14 00:44:37 +0530

    Enable WhiteNoise finders to resolve media image loading

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+WHITENOISE_USE_FINDERS = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),
