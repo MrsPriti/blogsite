4663dacc6196961d1be0caabe093dc7fe228b73f
Sourav Pradhan <sourava2345@gmail.com>
2026-05-14 20:50:12 +0530

    Switched Cloudinary credentials to environment variables for improved security.

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -158,9 +158,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dfhwdkwon',
-    'API_KEY': '417631771697518',
-    'API_SECRET': '9dxgENYOewKMgcTPTiIrDZIzh74',
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 # Default primary key field type
