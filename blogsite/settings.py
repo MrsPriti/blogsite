67c5cee64103856900fcbdb5df7a62a4a5cd26e9
Sourav Pradhan <sourava2345@gmail.com>
2026-05-14 20:51:01 +0530

    Simplified Cloudinary config to use a single CLOUDINARY_URL environment variable.

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -158,9 +158,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL'),
 }
 
 # Default primary key field type
